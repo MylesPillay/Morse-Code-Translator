ffe4c22592a3f4931aa27aba0376f87c4c674ba8
4naans <mylespillay@yahoo.co.uk>
2022-01-24 22:46:54 +0000

    added data

--- morse-code-translator/src/components/morsecode.jsx
+++ morse-code-translator/src/components/morsecode.jsx
@@ -1,3 +1,23 @@
+import { translateEnglishToMorse } from "./translator.js";
+import data from "../data";
+
+
+const inputEnglishWord = document.querySelector(".english-word");
+const submitEnglishWord = document.querySelector(".submit-btn");
+const receiveMorseTranslation = document.querySelector(".morse-translation");
+const reset = document.querySelector(".reset-btn");
+
+submitEnglishWord.addEventListener("click", () => {
+  const englishInput = inputEnglishWord.value;
+  const morseTranslation = translateEnglishToMorse(englishInput);
+  receiveMorseTranslation.innerHTML=`${morseTranslation}`;
+})
+
+reset.addEventListener("click", () => {
+  inputEnglishWord.value = "";
+  receiveMorseTranslation.innerHTML = "";
+})
+
 export const translateEnglishToMorse = (english) => {
     let englishArr = english.split("");
   
